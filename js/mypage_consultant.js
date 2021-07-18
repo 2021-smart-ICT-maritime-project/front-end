2d722f56e9dd84df65a043f32e161ad5e676a8a1
aimclee <dhy04057@likelion.org>
2021-07-18 20:52:51 +0900

    210718 commit

--- js/mypage_consultant.js
+++ js/mypage_consultant.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+    $('ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    })
 })
+
+if (window.location.hash == "#awaiting") {
+    var tab_id = "tab-2"
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $('#tab-awaiting').addClass('current');
+    $("#" + tab_id).addClass('current');
+}
