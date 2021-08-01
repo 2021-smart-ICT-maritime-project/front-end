c39b12585ac9ed3a05fa8b47e9be768a57be6d21
aimclee <dhy04057@likelion.org>
2021-08-01 23:44:54 +0900

    210801 commit

--- js/bg_video.js
+++
@@ -1,9 +0,0 @@
-const video_elem = document.querySelector('video');
-if (video_elem.requestFullscreen) {
-        video_elem.requestFullscreen();
-        } else if (video_elem.mozRequestFullScreen) {
-          video_elem.mozRequestFullScreen();
-        } else if (video_elem.webkitRequestFullscreen) {
-          video_elem.webkitRequestFullscreen();
-        }
-
